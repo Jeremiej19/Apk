10e3167a564c9748f60a595fba0f5abe6777833d
Jeremiej19 <jeremiej19@wp.pl>
2019-10-12 15:55:01 +0200

    spawn random

--- games/game1/game.js
+++ games/game1/game.js
@@ -1,7 +1,30 @@
+var width = 800;
+var height = 600;
+
+var objects = [];
+
+function spawn_enemy( i )
+{
+
+    var where =  parseInt(Math.random() * width );
+    console.log(where);
+    var object = document.createElement("img");
+    object.style = `position: absolute; left: ${where}px;`;
+    object.src = "bullseye1.png";
+    object.className = "trash";
+    
+    $("#board").append( object );
+    objects.push( object );
+    
+}
+
 $(document).ready(function(){
 
+    $("#board").css("width" , width + "px");
+    $("#board").css("height" , height + "px");
     $("#board").click(function(){
         console.log("a");
       });
+    setInterval( "spawn_enemy();" , 1000 )
   
   });
