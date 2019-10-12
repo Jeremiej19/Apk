6ca7c638d81aa50ea70fe7076bf7a9fef6662c1c
Jeremiej19 <jeremiej19@wp.pl>
2019-10-12 16:18:47 +0200

    onclick del

--- games/game1/game.js
+++ games/game1/game.js
@@ -1,30 +1,48 @@
 var width = 800;
 var height = 600;
 
+var img_width = 25 ;
+
 var objects = [];
 
 function spawn_enemy( i )
 {
 
-    var where =  parseInt(Math.random() * width );
+    var where =  parseInt(Math.random() * (width - img_width ) );
     console.log(where);
-    var object = document.createElement("img");
-    object.style = `position: absolute; left: ${where}px;`;
-    object.src = "bullseye1.png";
-    object.className = "trash";
+    var obj = document.createElement("img");
+    obj.style = `position: absolute; left: ${where}px; z-index:10;`;
+    obj.src = "bullseye1.png";
+    obj.onclick = function () {
+        console.log("b");
+        this.parentElement.removeChild(this);
+    };
+    obj.className = "trash";
     
-    $("#board").append( object );
-    objects.push( object );
+    $("#board").append( obj );
+    objects.push( obj );
     
+}
+
+function move_all()
+{
+    for( i in objects )
+    {
+        
+    }
 }
 
 $(document).ready(function(){
 
     $("#board").css("width" , width + "px");
     $("#board").css("height" , height + "px");
+
+    var spawn = setInterval( "spawn_enemy();" , 500 );
+    
     $("#board").click(function(){
         console.log("a");
+ //       clearInterval( spawn );
       });
-    setInterval( "spawn_enemy();" , 1000 )
+    
   
   });
