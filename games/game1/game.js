6b240509bbcd55c1c0935fde252e046e71551216
Jeremiej19 <jeremiej19@wp.pl>
2019-10-12 18:16:09 +0200

    animation,lives

--- games/game1/game.js
+++ games/game1/game.js
@@ -1,30 +1,58 @@
+var scoreboard_height = 20;
+
 var width = 800;
 var height = 600;
 
-var img_width = 25 ;
-var game_speed = 500;
+
+var img_size = 132;
+var spawn_delay = 1000;
+var object_animation_duration = 6000;
+
+var lives = 10;
+
 
 var objects = [];
 
+function delete_obj( obj )
+{
+    obj.parentElement.removeChild(obj);
+}
 
 function spawn_enemy( i )
 {
 
-    var where =  parseInt(Math.random() * (width - img_width ) );
-    console.log(where);
+    var where =  parseInt( Math.random() * ( width - img_size ) );
+//    console.log(where);
     var obj = document.createElement("img");
-    obj.style = `z-index:10; position: absolute; left: ${where}px;`;
-    obj.src = "bullseye1.png";
+    obj.style = `z-index:10; position: absolute; left: ${where}px; top:${scoreboard_height}px;`;
+    obj.src = "./butelka.png";
 
+//animacja
+    obj.setAttribute("onload",`$(this).animate({
+        top: ${ scoreboard_height + height - parseInt(img_size/2)} + 'px'
+    }, ${object_animation_duration},
+     'linear' ,
 
-    // obj.setAttribute("onmouseover",`$(this).animate({
-    //     left: 100 + 'px',
-    //     top: 32 + 'px'
-    // }, 400); `);
+     function(){
+        // console.log(this.parentElement);
+         if( this.parentElement == null)
+         {
+             return;
+         }
+        delete_obj(this);
+
+        $("#lives").html("${--lives}");
+        console.log(lives);
+        
+    
+    }); `);
+    ///nie klikniety
+        
 
     obj.onclick = function () {
-        console.log("b");
-        this.parentElement.removeChild(this);
+ //       console.log("enemy");
+       // this.parentElement.removeChild(this);
+       delete_obj(this);
     };
 
     obj.className = "trash";
@@ -41,10 +69,10 @@ $(document).ready(function(){
     $("#board").css("width" , width + "px");
     $("#board").css("height" , height + "px");
 
-    var spawn = setInterval( "spawn_enemy();" , game_speed );
+    var spawn = setInterval( "spawn_enemy();" , spawn_delay );
     
     $("#board").click(function(){
-        console.log("a");
+ //       console.log("board");
  //       clearInterval( spawn );
       });
     
